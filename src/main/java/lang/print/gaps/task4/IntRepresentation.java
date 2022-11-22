e3154c55cf130cfb775588db9ce119f8dcf61f04
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-22 23:37:36 +0400

    Changes

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int x = 63, y = 68, z = 61, q = 72;
+
+        System.out.println((char)63);
+        System.out.println((char)68);
+        System.out.println((char)61);
+        System.out.println((char)72);
+
     }
 }
